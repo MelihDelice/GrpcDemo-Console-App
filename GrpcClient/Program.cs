45367212e289c43f60dce3c8a7e9a268b1c162e1
MelihDelice <47330839+MelihDelice@users.noreply.github.com>
2021-07-05 19:52:04 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+using System;
+using System.Dynamic;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        private static string GrpcChannelAddress = "https://localhost:5001";
+        static async Task Main(string[] args)
+        {
+            #region Greeting
+            //var input = new HelloRequest { Name = "Melih" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+            #endregion
+            #region Customer
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Customer.CustomerClient(channel);
+            //var input = new CustomerLookupModel { UserId = 1 };
+
+            //var reply =await client.GetCustormerInfoAsync(input);
+            //Console.WriteLine(reply.FirstName+ " " + reply.LastName);
+            #endregion
+            var consoleInput = "";
+
+            while (true)
+            {
+                Console.WriteLine("Give Me Customer Id => {1,2,3}");
+                consoleInput = Console.ReadLine();
+                int possibleId = -1;
+                var isInt = int.TryParse(consoleInput, out possibleId);
+                if (isInt == false) Environment.Exit(0);
+
+                var channel = GrpcChannel.ForAddress(GrpcChannelAddress);
+                var client = new Customer.CustomerClient(channel);
+                var input = new CustomerLookupModel { UserId = possibleId };
+
+                var reply = await client.GetCustormerInfoAsync(input);
+                Console.WriteLine(reply.FirstName + " " + reply.LastName);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<CustomerModel> GetCustormerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else if (request.UserId == 3)
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"Requested Customer was not found! => ID : {request.UserId}");
+                Console.WriteLine();
+            }
+            if (output.FirstName != ""&& output.LastName != "")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Customer found! => ID : {request.UserId}");
+                Console.WriteLine();
+            }
+            return Task.FromResult(output);
+        }
+    }
+}
